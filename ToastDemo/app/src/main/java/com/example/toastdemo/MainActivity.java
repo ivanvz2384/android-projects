421f18d5e5300b41925f4cda18b07d049c7c3238
ivanvz2384 <ivanvz2384@gmail.com>
2019-10-19 00:13:20 -0500

    Commiting ToastDemo
    Toast Demo

---
+++ ToastDemo/app/src/main/java/com/example/toastdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.toastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doEnterName(View view) {
+        Log.i("Info", "Do enter name executed");
+        EditText nameEditText = findViewById(R.id.nameEditText);
+
+        Toast.makeText(MainActivity.this, "Hi there, " + nameEditText.getText().toString(), Toast.LENGTH_LONG);
+
+    }
+}
