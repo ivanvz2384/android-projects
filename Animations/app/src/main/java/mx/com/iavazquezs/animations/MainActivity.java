0e7e6a305bd16f5e8b0285919f7e0e153705ab86
Ivan Alonso Vazquez Serrano <iavazquezs@liverpool.com.mx>
2019-10-22 15:38:57 -0500

    Commit Animations part II
    Commit Animations part II

--- Animations/app/src/main/java/mx/com/iavazquezs/animations/MainActivity.java
+++ Animations/app/src/main/java/mx/com/iavazquezs/animations/MainActivity.java
@@ -3,6 +3,7 @@ package mx.com.iavazquezs.animations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,22 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView bartImageView = findViewById(R.id.bartImageView);
+        bartImageView.setTranslationX(-1000f);
+        bartImageView.setTranslationY(-1000f);
+
+
     }
 
     public void fade(View view) {
+        Log.i("INFO", "BART");
         ImageView bartImageView = findViewById(R.id.bartImageView);
-        bartImageView.animate().alpha(0f).setDuration(2000);
+
+        //bartImageView.animate().alpha(0f).setDuration(2000);
+        //bartImageView.setVisibility(View.INVISIBLE);
+
         //bartImageView.animate().translationYBy(1000f).setDuration(2000);
 
-        //ImageView homerImageView = findViewById(R.id.homerImageView);
-        //homerImageView.animate().alpha(1f).setDuration(2000);
+        //bartImageView.animate().translationXBy(1000f).setDuration(2000);
+        //bartImageView.animate().rotation(180f).setDuration(2000);
+
+        bartImageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+
+        /*
+        ImageView homerImageView = findViewById(R.id.homerImageView);
+        homerImageView.animate().alpha(1f).setDuration(2000);
+        homerImageView.setVisibility(View.VISIBLE);
+
+         */
     }
 
     public void fadeHomer(View view) {
+        Log.i("INFO", "HOMER");
         ImageView homerImageView = findViewById(R.id.homerImageView);
         homerImageView.animate().alpha(0f).setDuration(2000);
-
+        homerImageView.setVisibility(View.INVISIBLE);
         ImageView bartImageView = findViewById(R.id.bartImageView);
         bartImageView.animate().alpha(1f).setDuration(2000);
+        bartImageView.setVisibility(View.VISIBLE);
     }
 }
