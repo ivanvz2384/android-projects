9415fe415802c1026519df07bb1807840f26b35a
Ivan Alonso Vazquez Serrano <iavazquezs@liverpool.com.mx>
2019-11-01 17:33:49 -0600

    Language Preference
    Language Preference

---
+++ LanguagePreference/app/src/main/java/mx/com/iavazquezs/languagepreference/MainActivity.java
@@ -0,0 +1,77 @@
+package mx.com.iavazquezs.languagepreference;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = this.getSharedPreferences("mx.com.iavazquezs.languagepreference", Context.MODE_PRIVATE);
+        textView = findViewById(R.id.textView);
+
+        String language = sharedPreferences.getString("language", "");
+
+        if (language.isEmpty()) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose a language")
+                    .setMessage("What language would you like?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("English");
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("Spanish");
+                        }
+                    }).show();
+        } else {
+            textView.setText(language);
+        }
+    }
+
+    public void setLanguage(String language) {
+        sharedPreferences.edit().putString("language", language);
+        textView.setText(language);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.english) {
+            setLanguage("English");
+        } else if(item.getItemId() ==R.id.spanish ) {
+            setLanguage("Spanish");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
